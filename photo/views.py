770a836885727f5f465974bd657e311b52a2ab44
yuwook.kang <yuwook.kang@nhn.com>
2016-09-12 14:16:20 +0900

    포토 컨텐츠 편집 기능 작성 중...

--- photo/views.py
+++ photo/views.py
@@ -1,4 +1,5 @@
 from django.core.urlresolvers import reverse_lazy
+from django.views.generic import DeleteView
 from django.views.generic import ListView, DetailView, CreateView, UpdateView
 
 from mysite.views import LoginRequiredMixin
@@ -39,3 +40,79 @@ class PhotoUpdateView(LoginRequiredMixin, UpdateView):
     model = Photo
     fields = ['album', 'title', 'image', 'description']
     success_url = reverse_lazy('photo:index')
+
+
+class PhotoDeleteView(LoginRequiredMixin, DeleteView):
+    model = Photo
+    success_url = reverse_lazy('photo:index')
+
+
+class AlbumChangeLV(LoginRequiredMixin, ListView):
+    template_name = 'photo/album_change_list.html'
+
+    def get_queryset(self):
+        return Album.objects.filter(owner=self.request.user)
+
+
+class AlbumDeleteView(LoginRequiredMixin, DeleteView):
+    model = Album
+    success_url = reverse_lazy('photo:index')
+
+
+from django.shortcuts import redirect
+from photo.forms import PhotoInlineFormSet
+
+# InlineFormSet View
+class AlbumPhotoCV(LoginRequiredMixin, CreateView):
+    model = Album
+    fields = ['name', 'description']
+    template_name = 'photo/album_form.html'
+
+    def get_context_data(self, **kwargs): # 기본 컨텍스트 데이터 외에 추가적인 컨텍스트 변수를 설정할 때 오버라이딩
+        context = super(AlbumPhotoCV, self).get_context_data(**kwargs)
+        if self.request.POST:
+            context['formset'] = PhotoInlineFormSet(self.request.POST, self.request.FILES) # 포스트일 때는 왜 설정??
+        else:
+            context['formset'] = PhotoInlineFormSet() # 포토 추가/수정 할 수 있는 폼셋 추가
+        return context
+
+    def form_valid(self, form):
+        form.instance.owner = self.request.user
+        context = self.get_context_data()
+        formset = context['formset']
+
+        for photoform in formset:
+            photoform.instance.owner = self.request.user
+        if formset.is_valid():
+            self.object = form.save()
+            formset.instance = self.object
+            formset.save()
+            return redirect(self.object.get_absolute_url())
+        else:
+            return self.render_to_response(self.get_context_data(form=form))
+
+
+class AlbumPhotoUV(LoginRequiredMixin, UpdateView):
+    model = Album
+    fields = ['name', 'description']
+    template_name = 'photo/album_form.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(AlbumPhotoUV, self).get_context_data(**kwargs)
+        if self.request.POST:
+            context['formset'] = PhotoInlineFormSet(self.request.POST, self.request.FILES, instance=self.object)
+        else:
+            context['formset'] = PhotoInlineFormSet(instance=self.object)
+        return context
+
+    def form_valid(self, form):
+        context = self.get_context_data()
+        formset = context['formset']
+        if formset.is_valid():
+            self.object = form.save()
+            formset.instance = self.object
+            formset.save()
+            return redirect(self.object.get_absolute_url())
+        else:
+            return self.render_to_response(self.get_context_data(form=form))
+
